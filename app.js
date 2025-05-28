81dc6b343455006d6177cb50eda67dd5a09d8571
Jon Pater <jon@lendsum.com>
2025-05-28 12:20:37 +0200

    Stop throwing test error

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 5000
 app.use(express.static('dist'))
 
 app.listen(PORT, () => {
-  throw new Error(`Error starting server on port ${PORT}`)
+  console.log(`server started on port ${PORT}`)
 })
 
 app.get("/version", (req, res) => {
